99f87dbe3ca37da044b37779ed5e6b9342330bcc
sakirhussain <sakirhussainpa@gmail.com>
2025-07-07 17:16:09 +0530

    init commit

---
+++ src/main/java/com/example/equationservice/EquationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.equationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/equationservice/EquationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.equationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
